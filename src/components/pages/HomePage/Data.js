b0772d65ceb76ccd80cbf267a96dde5983b732a8
Shivam Prajapati <shivamprajapati.git@gmail.com>
2021-03-15 13:09:24 +0530

    update

--- src/components/pages/HomePage/Data.js
+++ src/components/pages/HomePage/Data.js
@@ -1,11 +1,12 @@
 export const obj = {
-    lightbg:false,
-    lightText: true,
-    lightTextDesc: true,
-    topLine: 'Welcome',
-    headline: 'Search colleges that you want',
-    description: 'Get complete details of colleges and students that you want',
-    buttonLabel:'Get Started',
-    imgStart:'',
-    img:'images/svg-1.svg',
-}
+  lightbg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Welcome",
+  headline: "Search colleges that you want",
+  description: "Get complete details of colleges and students that you want",
+  buttonLabel: "Get Started",
+  imgStart: "",
+  img:
+    "https://raw.githubusercontent.com/ShivamPrajapati-dev/OS_FE/main/public/images/svg-1.svg?token=AMXMEKFC4PJK66WUED7QP2LALBEWW",
+};
